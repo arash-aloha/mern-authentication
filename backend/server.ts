2f96de52a2f639e77ee04008b1e85f90fa4fd599
arash-aloha <arash.a@live.se>
2023-06-15 17:46:13 +0200

    new packages and implemented in server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,6 @@
 import express, { Application, NextFunction, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./db/database";
 import UserRoutes from "./routes/userRoutes";
@@ -7,13 +9,15 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: "./config/.env" });
 
 const app: Application = express();
-const PORT = process.env.PORT;
+const PORT: string = process.env.PORT;
 
 app.use(express.json());
+app.use(cors({ credentials: true }));
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log({ path: req.path, method: req.method });
   next();
 });
+app.use(cookieParser());
 
 app.use("/api/users", UserRoutes);
 
