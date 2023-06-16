c99f3f720d2d75b87686a6e4cf8854824258f74b
arash-aloha <arash.a@live.se>
2023-06-16 10:45:23 +0200

    new helper and actions

--- backend/src/controllers/authentication.ts
+++ backend/src/controllers/authentication.ts
@@ -5,11 +5,27 @@ import UserModel, {
   IUserDocument,
   IUserInput,
 } from "../models/UserModel";
+import { getUserByEmail } from "./actions";
 
-export const register = async (req: Request, res: Response) => {
+export const signupNewUser = async (req: Request, res: Response) => {
   try {
     const { email, password, firstName } = req.body;
+    if (checkBodyIfNullOrUndefined(email, password, firstName)) {
+      return res.sendStatus(400);
+    }
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return res.sendStatus(409);
+    }
+    console.log("user does not exist");
   } catch (error) {
     return res.status(500).send({ "Register error": error });
   }
 };
+
+export function checkBodyIfNullOrUndefined(email, password, firstName) {
+  if (!email) return `something wrong with email: ${email}`;
+  if (!password) return `something wrong with email: ${password}`;
+  if (!firstName) return `something wrong with firstName: ${firstName}`;
+}
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,33 +1,30 @@
-// import { Request, Response, Router } from "express";
-// import {
-//   invokeLoginUser,
-//   invokeSignupNewUser,
-// } from "../controllers/authentication";
+import { Request, Response, Router } from "express";
+import { signupNewUser } from "../controllers/authentication";
 
-// const UserRoutes: Router = Router();
+const UserRoutes: Router = Router();
 
-// UserRoutes.post("/login", invokeLoginUser);
+UserRoutes.post("/login");
 
-// UserRoutes.post("/signup", invokeSignupNewUser);
+UserRoutes.post("/signup", signupNewUser);
 
-// UserRoutes.get("/", (req: Request, res: Response) => {
-//   res.send({ res: "GET all users" });
-// });
+UserRoutes.get("/", (req: Request, res: Response) => {
+  res.send({ res: "GET all users" });
+});
 
-// UserRoutes.get("/:id", (req: Request, res: Response) => {
-//   res.send({ res: "Get single user" });
-// });
+UserRoutes.get("/:id", (req: Request, res: Response) => {
+  res.send({ res: "Get single user" });
+});
 
-// UserRoutes.post("/", (req: Request, res: Response) => {
-//   res.send({ res: "Add a new user" });
-// });
+UserRoutes.post("/", (req: Request, res: Response) => {
+  res.send({ res: "Add a new user" });
+});
 
-// UserRoutes.patch("/:id", (req: Request, res: Response) => {
-//   res.send({ res: "Change user detail" });
-// });
+UserRoutes.patch("/:id", (req: Request, res: Response) => {
+  res.send({ res: "Change user detail" });
+});
 
-// UserRoutes.delete("/:id", (req: Request, res: Response) => {
-//   res.send({ res: "Delete user" });
-// });
+UserRoutes.delete("/:id", (req: Request, res: Response) => {
+  res.send({ res: "Delete user" });
+});
 
-// export default UserRoutes;
+export default UserRoutes;
