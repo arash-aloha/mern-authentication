17f7f9ee376f82d2b1a5957beb825624e4490e05
arash-aloha <arash.a@live.se>
2023-06-19 14:44:08 +0200

    new settings in nodemon.json and package.json. Adding helpers and actions

--- backend/src/controllers/authentication.ts
+++ backend/src/controllers/authentication.ts
@@ -9,12 +9,12 @@ import { getUserByEmail } from "./actions";
 
 export const signupNewUser = async (req: Request, res: Response) => {
   try {
-    const { email, password, firstName } = req.body;
-    if (checkBodyIfNullOrUndefined(email, password, firstName)) {
+    const { res, email, password, firstName } = req.body;
+    if (checkBodyIfNullOrUndefined(res, email, password, firstName)) {
       return res.sendStatus(400);
     }
-    const existingUser = await getUserByEmail(email);
 
+    const existingUser = await getUserByEmail(email);
     if (existingUser) {
       return res.sendStatus(409);
     }
@@ -24,8 +24,18 @@ export const signupNewUser = async (req: Request, res: Response) => {
   }
 };
 
-export function checkBodyIfNullOrUndefined(email, password, firstName) {
-  if (!email) return `something wrong with email: ${email}`;
-  if (!password) return `something wrong with email: ${password}`;
-  if (!firstName) return `something wrong with firstName: ${firstName}`;
+export function checkBodyIfNullOrUndefined(
+  res: Response,
+  email: string,
+  password: string,
+  firstName: string
+) {
+  if (!email)
+    return res.status(400).send(`something wrong with email: ${email}`);
+  if (!password)
+    return res.status(400).send(`something wrong with password: ${password}`);
+  if (!firstName)
+    return res
+      .status(400)
+      .send(`something wrong with first name: ${firstName}`);
 }
--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -1,39 +1,10 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
 import * as mongoDB from "mongodb";
 import * as dotenv from "dotenv";
 
-// export async function connectDB() {
-//   dotenv.config({ path: "./config/.env" });
-//   const uri: string = process.env.MONGO_URI;
-//   const client: MongoClient = new MongoClient(uri, {
-//     serverApi: {
-//       version: ServerApiVersion.v1,
-//       strict: true,
-//       deprecationErrors: true,
-//     },
-//   });
-
-//   try {
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } catch (err) {
-//     console.error("Failed to connect to MongoDB:", {
-//       error_message: err.message,
-//       error: err,
-//     });
-//   } finally {
-//     await client.close();
-//   }
-// }
-
 export const collections: { users?: mongoDB.Collection } = {};
-
 export async function connectDB() {
-  dotenv.config({ path: "./config/.env" });
+  dotenv.config({ path: "src/config/.env" });
+
   const uri: string = process.env.MONGO_URI;
   const databaseName = process.env.DB;
   const collectionName = process.env.COLLECTION;
